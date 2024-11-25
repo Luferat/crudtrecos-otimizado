c63a41e37a44a717a21454c04db2fcb579849b7a
André Luferat <contato@luferat.net>
2024-11-25 16:05:06 -0300

    otimização

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, abort, g, make_response, redirect, render_template, req
 from flask_mysqldb import MySQL
 import json
 from functions.geral import calcular_idade, datetime_para_string, gerar_senha, remove_prefixo
+from modules.apaga import mod_apaga
+from modules.edita import mod_edita
+from modules.index import mod_index
+from modules.novo import mod_novo
+from modules.page_not_found import mod_page_not_found
+from modules.start import mod_start
 
 
 # Cria um aplicativo Flask chamado "app"
@@ -25,200 +31,28 @@ mysql = MySQL(app)
 
 @app.before_request
 def start():
-
-    # Setup do MySQL para corrigir acentuação
-    cur = mysql.connection.cursor()
-    cur.execute("SET NAMES utf8mb4")
-    cur.execute("SET character_set_connection=utf8mb4")
-    cur.execute("SET character_set_client=utf8mb4")
-    cur.execute("SET character_set_results=utf8mb4")
-
-    # Setup do MySQL para dias da semana e meses em português
-    cur.execute("SET lc_time_names = 'pt_BR'")
-
-    # Lê o cookie do usuário → 'usuario'
-    cookie = request.cookies.get('usuario')
-
-    if cookie:
-        # Se o cookie existe, Converte o valor dele de JSON para dicionário
-        g.usuario = json.loads(cookie)
-    else:
-        # Se o cookie não existe, a variável do ususário está vazia
-        g.usuario = ''
+    mod_start(mysql=mysql)
 
 
 @app.route("/")  # Rota raiz, equivalente a página inicial do site (index)
 def index():  # Função executada ao acessar a rota raiz
-
-    # Se o usuário não está logado redireciona para a página de login
-    if g.usuario == '':
-        return redirect(url_for('login'))
-
-    # Recebe valor da querystring, se existir → /?a=xxxxx
-    acao = request.args.get('a')
-
-    # Obtém todos os 'trecos' do usuário conectado
-    sql = '''
-        SELECT t_id, t_foto, t_nome, t_descricao, t_localizacao
-        FROM treco
-        WHERE t_usuario = %s
-            AND t_status = 'on'
-        ORDER BY t_data DESC
-    '''
-    cur = mysql.connection.cursor()
-    cur.execute(sql, (g.usuario['id'],))
-    rows = cur.fetchall()
-    cur.close()
-
-    # Teste de mesa para verificar o retorno dos dados do banco de dados
-    # print('\n\n\n DB:', rows, '\n\n\n')
-
-    # Dados, variáveis e valores a serem passados para o template HTML
-    pagina = {
-        'titulo': 'CRUDTrecos',  # ← 'titulo' é obrigatório para todas as páginas / rotas
-        'usuario': g.usuario,  # ← 'usuario' é obrigatório para todas as páginas / rotas
-        'items': rows,
-        'acao': acao,
-    }
-
-    # Renderiza o template HTML, passando valores (pagina) para ele
-    return render_template('index.html', **pagina)
+    return mod_index(mysql=mysql)
 
 
 # Rota para a página de cadastro de novo treco
 @app.route('/novo', methods=['GET', 'POST'])
 def novo():  # Função executada para cadastrar novo treco
-
-    # Se o usuário não está logado redireciona para a página de login
-    if g.usuario == '':
-        return redirect(url_for('login'))
-
-    # Variável que ativa a mensagem de sucesso no HTML
-    sucesso = False
-
-    # Se o formulário foi enviado
-    if request.method == 'POST':
-
-        # Obtém os dados preenchidos na forma de dicionário
-        form = dict(request.form)
-
-        # Teste de mesa (comente depois dos testes)
-        # Verifica se os dados do formulário chegaram ao back-end
-        # print('\n\n\n FORM:', form, '\n\n\n')
-
-        # Grava os dados no banco de dados
-        sql = '''
-            INSERT INTO treco (
-                t_usuario, t_foto, t_nome, t_descricao, t_localizacao
-            ) VALUES (
-                %s, %s, %s, %s, %s
-            )
-        '''
-        cur = mysql.connection.cursor()
-        cur.execute(sql, (
-            g.usuario['id'],
-            form['foto'],
-            form['nome'],
-            form['descricao'],
-            form['localizacao'],
-        ))
-        mysql.connection.commit()
-        cur.close()
-
-        sucesso = True
-
-    # Dados, variáveis e valores a serem passados para o template HTML
-    pagina = {
-        'titulo': 'CRUDTrecos - Novo Treco',
-        'usuario': g.usuario,
-        'sucesso': sucesso,
-    }
-
-    # Renderiza o template HTML, passaod valores para ele
-    return render_template('novo.html', **pagina)
+    return mod_novo(mysql=mysql)
 
 
 @app.route('/edita/<id>', methods=['GET', 'POST'])
 def edita(id):
-
-    # Se o usuário não está logado redireciona para a página de login
-    if g.usuario == '':
-        return redirect(url_for('login'))
-
-    # Se o formulário foi enviado
-    if request.method == 'POST':
-        form = dict(request.form)
-
-        # print('\n\n\n FORM:', form, '\n\n\n')
-
-        sql = '''
-            UPDATE treco 
-            SET t_foto = %s,
-                t_nome = %s,
-                t_descricao = %s,
-                t_localizacao = %s
-            WHERE t_id = %s
-        '''
-        cur = mysql.connection.cursor()
-        cur.execute(sql, (
-            form['foto'],
-            form['nome'],
-            form['descricao'],
-            form['localizacao'],
-            id,
-        ))
-        mysql.connection.commit()
-        cur.close()
-
-        # Após editar, retorna para a lista de itens
-        return redirect(url_for('index', a='editado'))
-
-    sql = '''
-        SELECT * FROM treco
-        WHERE t_id = %s
-            AND t_usuario = %s
-            AND t_status = 'on'
-    '''
-    cur = mysql.connection.cursor()
-    cur.execute(sql, (id, g.usuario['id'],))
-    row = cur.fetchone()
-    cur.close()
-
-    # print('\n\n\n DB:', row, '\n\n\n')
-
-    if row == None:
-        abort(404)
-
-    pagina = {
-        'titulo': 'CRUDTrecos',
-        'usuario': g.usuario,
-        'item': row,
-    }
-
-    return render_template('edita.html', **pagina)
+    return mod_edita(mysql=mysql, id=id)
 
 
 @app.route('/apaga/<id>')
 def apaga(id):
-
-    # Se o usuário não está logado redireciona para a página de login
-    if g.usuario == '':
-        return redirect(url_for('login'))
-
-    # (des)comente o método para apagar conforme o seu caso
-    # Apaga completamente o treco (CUIDADO!)
-    # sql = 'DELETE FROM treco WHERE t_id = %s'
-    # Altera o status do treco para 'del' (Mais seguro)
-    sql = "UPDATE treco SET t_status = 'del'  WHERE t_id = %s"
-
-    # Executa o SQL
-    cur = mysql.connection.cursor()
-    cur.execute(sql, (id,))
-    mysql.connection.commit()
-    cur.close()
-
-    # Retorna para a lista de items
-    return redirect(url_for('index', a='apagado'))
+    return mod_apaga(mysql, id)
 
 
 @app.route('/login', methods=['GET', 'POST'])  # Rota para login de usuário
@@ -580,11 +414,7 @@ def editaperfil():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    pagina = {
-        'titulo': 'CRUDTrecos - Erro 404',
-        'usuario': g.usuario,
-    }
-    return render_template('404.html', **pagina), 404
+    return mod_page_not_found()
 
 
 # Executa o servidor HTTP se estiver no modo de desenvolvimento
